999d2bdc1f8ed04021ece546ca351230d5b3a6f5
kallolghosal <kghosal@gmail.com>
2023-12-18 16:35:21 +0530

    test jalal

--- app/components/bomebanner.js
+++ app/components/bomebanner.js
@@ -8,24 +8,22 @@ export default function homebanner () {
             <Carousel.Item>
                 <Container fluid className="homebanner">
                     <Row className="g-0 w-100">
-                     <Col><Image src={'/img/homebanner-1.jpeg'} width={500} height={700} className="img-fluid" alt="Home Banner"/></Col>   
-                     <Col className="">
-                     <div className="centerbox w-100">
-                        <p style={{fontWeight:'bold'}}>COVID-19 RESPONSE UPDATES</p>
-                        <div className="numcounterarea">
-                            <div id="numcounter"></div>
-                            <p style={{color:'#e7ba48', fontSize:'22px', fontWeight:'bold', margin:'0px'}}>HOUSEHOLDS</p>
-                        </div>
-                        <p style={{color:'white'}}>provided health advisory, awareness, food supplies, medical kits and financial services</p>
-                        <Button href={'https://pages.razorpay.com/vrutticovid19'} style={{backgroundColor:'#f05922', border:'none'}}>Donate Now</Button>
-                    </div>
-                     </Col>
-                     <Col><Image src={'/img/homebanner-2.jpeg'} width={500} height={700} alt="Home Banner"></Image></Col>
+                        <Col><Image src={'/img/homebanner-1.jpeg'} width={500} height={700} className="img-fluid" alt="Home Banner"/></Col>   
+                        <Col className="">
+                            <div className="left-circle"></div>
+                            <div className="centerbox w-100">
+                                <p style={{fontWeight:'bold'}}>COVID-19 RESPONSE UPDATES</p>
+                                <div className="numcounterarea">
+                                    <div id="numcounter"></div>
+                                    <p style={{color:'#e7ba48', fontSize:'22px', fontWeight:'bold', margin:'0px'}}>HOUSEHOLDS</p>
+                                </div>
+                                <p style={{color:'white'}}>provided health advisory, awareness, food supplies, medical kits and financial services</p>
+                                <Button href={'https://pages.razorpay.com/vrutticovid19'} style={{backgroundColor:'#f05922', border:'none'}}>Donate Now</Button>
+                            </div>
+                            <div className="right-circle"></div>
+                        </Col>
+                        <Col><Image src={'/img/homebanner-2.jpeg'} width={500} height={700} alt="Home Banner"></Image></Col>
                     </Row>
-
-                    
-                   
-                    
                 </Container>
             </Carousel.Item>
 
@@ -44,6 +42,7 @@ export default function homebanner () {
                     <Image src={'/img/homebanner-22.jpeg'} width={500} height={700} alt="Home Banner"></Image>
                 </Container>
             </Carousel.Item>
+
             <Carousel.Item>
                 <Container fluid className="homebanner">
                     <Image src={'/img/homebanner-13.jpeg'} width={500} height={700} alt="Home Banner"></Image>
@@ -59,6 +58,7 @@ export default function homebanner () {
                     <Image src={'/img/homebanner-33.jpeg'} width={500} height={700} alt="Home Banner"></Image>
                 </Container>
             </Carousel.Item>
+
             <Carousel.Item>
                 <Container fluid className="homebanner">
                     <Image src={'/img/homebanner-14.jpeg'} width={500} height={700} alt="Home Banner"></Image>
@@ -74,6 +74,7 @@ export default function homebanner () {
                     <Image src={'/img/homebanner-44.jpeg'} width={500} height={700} alt="Home Banner"></Image>
                 </Container>
             </Carousel.Item>
+
         </Carousel>
     );
 }
